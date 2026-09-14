1e948fe1a45a4963309f74d28788bd22046bc0ae
Heush256 <ktoromani10@gmail.com>
2026-09-14 09:22:37 -0400

    Delete Get_and_Run_Info directory

--- Get_and_Run_Info/Run/Run_Process.h
+++
@@ -1,6 +0,0 @@
-#ifndef MKV_READER_RUN_PROCESS_H
-#define MKV_READER_RUN_PROCESS_H
-
-int MKV_merge_process(const Episode* Names, const struct User_Inputs* User);
-
-#endif //MKV_READER_RUN_PROCESS_H
